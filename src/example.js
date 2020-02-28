4e93ab75dd26a18ac5a5fae8d337243d2bea144d
Jorge Aguiar Martin <jotamusik@gmail.com>
2020-02-28 18:07:24 +0000

    asdf

--- src/example.js
+++ src/example.js
@@ -7,8 +7,4 @@ export default class Example {
   getExampleVariable() {
     return this.exampleVariable;
   }
-
-  setExampleVariable(exampleVariable) {
-    this.exampleVariable = exampleVariable;
-  }
 }
